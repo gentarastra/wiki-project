f0c850750ce52a971e759ba4b7a18235b70c24bf
gentarastra <gentarastra@gmail.com>
2026-03-18 09:16:06 +0700

    Update app.js

--- wikipedia-frontend/js/app.js
+++ wikipedia-frontend/js/app.js
@@ -1,6 +1,6 @@
 // wikipedia-frontend/js/app.js
 
-const SOCKET_URL = "103.189.201.108"; 
+const SOCKET_URL = "https://shaky-nails-search.loca.lt"; 
 const socket = io(SOCKET_URL);
 
 // --- 0. DEKLARASI SEMUA ELEMEN ---
